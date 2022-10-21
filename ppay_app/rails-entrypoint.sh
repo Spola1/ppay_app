594529aac68e98230326e1597dba518fb6415e37
Evgeny S <e.g.sokolov@gmail.com>
2022-10-21 16:09:50 +0300

    docker - try to change

--- ppay_app/rails-entrypoint.sh
+++ ppay_app/rails-entrypoint.sh
@@ -1,16 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 set -e
 
+# ------ #
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
-
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+# ------ #
 
-#bundle exec whenever --update-crontab
-#crontab -l
+# Always keep this here as it ensures your latest built assets make their way
+# into your volume persisted public directory.
+cp -r /public /app
 
-bundle exec rails server Puma -b 0.0.0.0
+# Sprockets will use the first sprockets file it finds not the latest one. We
+# need to delete all of the old sprockets files except for the one that was
+# last built into the image. That's what the code below does.
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# shellcheck disable=SC2125
+manifest_files=/app/public/assets/.sprockets-manifest-*.json
+
+if compgen -G "${manifest_files}" > /dev/null 2>&1; then
+  # shellcheck disable=SC2086,SC2061
+  find \
+    ${manifest_files} \
+    -type f ! -name "$(basename /public/assets/.sprockets-manifest-*.json)" \
+    -delete
+fi
+
 exec "$@"
-
