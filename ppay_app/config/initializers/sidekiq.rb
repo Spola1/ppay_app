b728a34a1121755535fc145f1cd8f01a8f9f6b79
Evgeny S <e.g.sokolov@gmail.com>
2022-10-25 14:39:45 +0300

    redis - small fix
    поменял ссылку на redis
    для сайдкика (не экшн кэйбл)

--- ppay_app/config/initializers/sidekiq.rb
+++ ppay_app/config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
-sidekiq_config = { url: ENV['REDIS_HOST'].present? ? "redis://#{ ENV['REDIS_HOST'] }:6379" : 'redis://redis:6379/0' }
+sidekiq_config = { url: ENV['REDIS_HOST'].present? ? "redis://#{ ENV['REDIS_HOST'] }:6379/0" : 'redis://redis:6379/0' }
+
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
 end
