e2c3216af99c98ed66e3c5627fbc4ed3c91d84d2
Evgeny S <e.g.sokolov@gmail.com>
2022-11-24 08:39:36 +0300

    config.time_zone = 'Moscow'

--- ppay_app/config/application.rb
+++ ppay_app/config/application.rb
@@ -19,6 +19,7 @@ module PpayApp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Moscow'
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators.test_framework = :rspec
   end
