e3ed814be74d7eb0f91134dd0f33931fae4c573e
Evgeny S <e.g.sokolov@gmail.com>
2022-12-04 15:42:20 +0300

    try to fix ActionCable
    еще добавил в .env файл локально и на сервере переменную:
    ENV['APP_DOMAIN']

--- ppay_app/config/environments/production.rb
+++ ppay_app/config/environments/production.rb
@@ -46,6 +46,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ ENV['APP_DOMAIN'] ]
+  config.hosts << ENV['APP_DOMAIN']
+  config.hosts << "localhost"
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
