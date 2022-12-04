8dfb70a9872eb86e142256de8675688f5e109524
Evgeny S <e.g.sokolov@gmail.com>
2022-12-04 16:11:15 +0300

    try to fix - action cable
    все равно белый экран
    убираю две строки

--- ppay_app/config/environments/production.rb
+++ ppay_app/config/environments/production.rb
@@ -47,8 +47,7 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = [ ENV['APP_DOMAIN'] ]
-  config.hosts << ENV['APP_DOMAIN']
-  config.hosts << "localhost"
+
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
