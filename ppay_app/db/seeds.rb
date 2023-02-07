00a3db43b21f617fe568688000c751736774d8de
Evgeniy Stashkovyan <stashevgenij@yandex.ru>
2023-02-07 15:12:18 +0300

    rubocop

--- ppay_app/db/seeds.rb
+++ ppay_app/db/seeds.rb
@@ -13,7 +13,8 @@ ExchangePortal.create(name: 'Binance P2P')
 
 u1 = Admin.create(email: 'admin@test.com', password: 'NQg6By9QncR5KssZ', nickname: 'SuperAdmin', role: 'superadmin',
                   name: 'Анатолий')
-u2 = Merchant.create(email: 'merchant@test.com', password: 'NQg6By9QncR5KssZ', nickname: 'AvangardBet', name: 'Петр Петрович')
+u2 = Merchant.create(email: 'merchant@test.com', password: 'NQg6By9QncR5KssZ', nickname: 'AvangardBet',
+                     name: 'Петр Петрович')
 u3 = Processer.create(email: 'processer1@test.com', password: 'NQg6By9QncR5KssZ', nickname: 'VasiaBTC', name: 'Вася')
 u4 = Processer.create(email: 'processer2@test.com', password: 'NQg6By9QncR5KssZ', nickname: 'IvanCrypto', name: 'Ваня')
 u5 = Support.create(email: 'support@test.com', password: 'NQg6By9QncR5KssZ', nickname: 'Svetlana911', name: 'Светлана')
