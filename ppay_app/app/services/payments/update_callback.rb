8efc34ad3828d22ba65e824f0272dccba68c8f53
mveynberg <mv@cj264.ru>
2023-02-09 12:30:35 +0500

    Merchant's callback POST request headers should contain Authorization

--- ppay_app/app/services/payments/update_callback.rb
+++ ppay_app/app/services/payments/update_callback.rb
@@ -4,19 +4,23 @@ require 'net/http'
 
 module Payments
   class UpdateCallback < ApplicationService
-    attr_reader :callback_url, :uuid, :external_order_id, :payment_status
+    attr_reader :callback_url, :uuid, :external_order_id, :payment_status, :token
 
     def initialize(payment)
       @callback_url = payment.callback_url
       @uuid = payment.uuid
       @external_order_id = payment.external_order_id
       @payment_status = payment.payment_status
+      @token = payment.merchant.token
     end
 
     def call
       uri = URI(callback_url)
       body = { uuid:, external_order_id:, payment_status: }
-      headers = { 'Content-Type': 'application/json' }
+      headers = {
+        'Content-Type':  'application/json',
+        'Authorization': "Bearer #{token}"
+      }
       Net::HTTP.post(uri, body.to_json, headers)
     end
   end
