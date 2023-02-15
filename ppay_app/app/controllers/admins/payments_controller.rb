7323bfb1e0180e81e56261c3d5d32eb84b9b67f4
Spola1 <1111.22224@mail.ru>
2023-02-15 11:36:44 +0300

    copy supprot views&&controllers to admin

--- ppay_app/app/controllers/admins/payments_controller.rb
+++ ppay_app/app/controllers/admins/payments_controller.rb
@@ -2,9 +2,32 @@
 
 module Admins
   class PaymentsController < Staff::BaseController
+    before_action :find_payment, only: %i[update show]
+
     def index
-      @pagy, @payments = pagy(Payment.includes(:merchant).order(created_at: :desc))
+      @pagy, @payments = pagy(Payment.includes(:merchant))
       @payments = @payments.decorate
+
+      @arbitration_payments_pagy, @arbitration_payments = pagy(Payment.arbitration.includes(:merchant), page_param: :arbitration_page)
+      @arbitration_payments = @arbitration_payments.decorate
+    end
+
+    def show; end
+
+    def update
+      @payment.update(payment_params)
+
+      render :show
+    end
+
+    private
+
+    def find_payment
+      @payment = Payment.find_by(uuid: params[:uuid]).becomes(model_class.constantize).decorate
+    end
+
+    def payment_params
+      required_params.permit(:payment_status, :arbitration, :cancellation_reason)
     end
   end
 end
--- ppay_app/config/routes.rb
+++ ppay_app/config/routes.rb
@@ -32,7 +32,11 @@ Rails.application.routes.draw do
     resources :transactions, only: %i[index show]
     resources :balance_requests
     resources :payments, param: :uuid, only: %i[index update show]
-    root 'transactions#index', as: :admins_root
+    namespace :payments do
+      resources :deposits, param: :uuid, only: %i[index update show edit]
+      resources :withdrawals, param: :uuid, only: %i[index update show edit]
+    end
+    root 'payments#index', as: :admins_root
   end
 
   scope module: :merchants, constraints: ->(request) { request.env['warden'].user&.merchant? } do
