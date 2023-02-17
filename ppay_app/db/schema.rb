e3b071fb3d308d833734b71196fbfb167a8d8f5a
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-02-17 16:13:52 +0600

    add initial amount to payment model

--- ppay_app/db/schema.rb
+++ ppay_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_223251) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_094834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -188,6 +188,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_223251) do
     t.string "callback_url"
     t.integer "cancellation_reason"
     t.integer "unique_amount"
+    t.decimal "initial_amount", precision: 12, scale: 2
     t.index ["support_id"], name: "index_payments_on_support_id"
   end
 
