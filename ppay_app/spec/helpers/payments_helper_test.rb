3f3528984b14a08098012f62bfc6e21d9d6c8af1
Spola1 <1111.22224@mail.ru>
2023-02-21 15:44:37 +0300

    update test

--- ppay_app/spec/helpers/payments_helper_test.rb
+++ ppay_app/spec/helpers/payments_helper_test.rb
@@ -2,13 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PaymentsHelper, type: :helper do
   describe "#support_payment_statuses_collection" do
+    let(:expected_collection) { [["Перевод денег", :transferring], ["Подтверждение перевода", :confirming],
+                                   ["Успешно завершён", :completed], ["Отменён", :cancelled]] }
+
     it "returns an array of arrays with status translations and symbols" do
-      expected_collection = [
-        ["Перевод денег", :transferring],
-        ["Подтверждение перевода", :confirming],
-        ["Успешно завершён", :completed],
-        ["Отменён", :cancelled]
-      ]
       expect(helper.support_payment_statuses_collection).to eq(expected_collection)
     end
   end
