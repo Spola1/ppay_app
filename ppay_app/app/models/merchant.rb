c3d6fc71c12287aaac0e8a989b4acc35aa2915d6
mveynberg <mv@cj264.ru>
2023-03-09 21:58:03 +0500

    Merchant fill_in_commissions after_create

--- ppay_app/app/models/merchant.rb
+++ ppay_app/app/models/merchant.rb
@@ -14,4 +14,23 @@ class Merchant < User
     integer: 1,
     decimal: 2
   }, _prefix: true
+
+  after_create :fill_in_commissions
+
+  def fill_in_commissions
+    commissions.create(all_possible_commissions)
+  end
+
+  private
+
+  def all_possible_commissions
+    [{ commission: 1 }]
+      .product(
+        PaymentSystem.all.map { { payment_system: _1 } },
+        Settings.national_currencies.map { { national_currency: _1 } },
+        %w[Deposit Withdrawal].map { { direction: _1 } },
+        %i[ppay processer working_group agent].map { { commission_type: _1 } }
+      )
+      .map { _1.inject(:merge) }
+  end
 end
--- ppay_app/db/data/20230309065121_add_commissions.rb
+++ ppay_app/db/data/20230309065121_add_commissions.rb
@@ -2,21 +2,6 @@
 
 class AddCommissions < ActiveRecord::Migration[7.0]
   def up
-    Merchant.find_each do |merchant|
-      merchant.commissions.create(
-        all_possible_commissions
-      )
-    end
-  end
-
-  def all_possible_commissions
-    payment_systems = PaymentSystem.all.map { { payment_system: _1 } }
-    national_currencies = Settings.national_currencies.map { { national_currency: _1 } }
-    directions = %w[Deposit Withdrawal].map { { direction: _1 } }
-    commission_types = %i[ppay processer working_group agent].map { { commission_type: _1 } }
-
-    [{ commission: 1 }]
-      .product(payment_systems, national_currencies, directions, commission_types)
-      .map { _1.inject(:merge) }
+    Merchant.find_each { |merchant| merchant.fill_in_commissions }
   end
 end
