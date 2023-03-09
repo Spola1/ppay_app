e4e03d9f1442c6bfa5d79268079a733dd02ad7d7
mveynberg <mv@cj264.ru>
2023-03-09 13:00:52 +0500

    Rubocop fixes

--- ppay_app/db/data/20230309065121_add_commissions.rb
+++ ppay_app/db/data/20230309065121_add_commissions.rb
@@ -2,7 +2,7 @@
 
 class AddCommissions < ActiveRecord::Migration[7.0]
   def up
-    Merchant.all.each do |merchant|
+    Merchant.find_each do |merchant|
       merchant.commissions.create(
         all_possible_commissions
       )
@@ -10,12 +10,12 @@ class AddCommissions < ActiveRecord::Migration[7.0]
   end
 
   def all_possible_commissions
-    payment_systems = PaymentSystem.all.map { {payment_system: _1} }
-    national_currencies = Settings.national_currencies.map { {national_currency: _1} }
-    directions = %w[Deposit Withdrawal].map { {direction: _1} }
-    commission_types = %i[ppay processer working_group agent].map { {commission_type: _1} }
+    payment_systems = PaymentSystem.all.map { { payment_system: _1 } }
+    national_currencies = Settings.national_currencies.map { { national_currency: _1 } }
+    directions = %w[Deposit Withdrawal].map { { direction: _1 } }
+    commission_types = %i[ppay processer working_group agent].map { { commission_type: _1 } }
 
-    [{commission: 1}]
+    [{ commission: 1 }]
       .product(payment_systems, national_currencies, directions, commission_types)
       .map { _1.inject(:merge) }
   end
