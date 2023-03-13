fe15bbb4dff5fbf3104cde5ba9a2327d3b311398
mveynberg <mv@cj264.ru>
2023-03-13 21:59:17 +0500

    Correct audits helper with spec

--- ppay_app/spec/helpers/audits_helper_spec.rb
+++ ppay_app/spec/helpers/audits_helper_spec.rb
@@ -6,21 +6,41 @@ RSpec.describe AuditsHelper, type: :helper do
   describe '#formatted_changes' do
     subject { formatted_changes(attribute, values) }
 
-    context 'when attribute is not "status_changed_at"' do
-      let(:attribute) { 'payment_status' }
+    let(:attribute) { 'payment_status' }
+
+    context 'values is an array' do
       let(:values) { %w[transferring confirming] }
 
-      it 'returns values joined with " -> "' do
-        expect(subject).to eq('transferring -> confirming')
+      it 'returns elements joined with " -> "' do
+        expect(subject).to eq(values.join(' -> '))
       end
     end
 
-    context 'when attribute is "status_changed_at"' do
-      let(:attribute) { 'status_changed_at' }
-      let(:values) { ['2023-02-02TO2:46:30.352+03:00', '2023-02-13T19:59:17.202+03:00'] }
+    context 'values is not an array' do
+      let(:values) { 'created' }
 
-      it 'returns formatted values joined with " -> "' do
-        expect(subject).to eq('2023-02-02 02:46 -> 2023-02-13 19:59')
+      it 'returns its content' do
+        expect(subject).to eq values
+      end
+    end
+
+    context 'attribute is "status_changed_at"' do
+      let(:attribute) { 'status_changed_at' }
+
+      context 'values is single string' do
+        let(:values) { '2023-02-02TO2:46:30.352+03:00' }
+
+        it 'returns formatted datetime' do
+          expect(subject).to eq('2023-02-02 02:46')
+        end
+      end
+
+      context 'values is an array' do
+        let(:values) { ['2023-02-02TO2:46:30.352+03:00', '2023-02-13T19:59:17.202+03:00'] }
+
+        it 'returns formatted datetimes' do
+          expect(subject).to eq('2023-02-02 02:46 -> 2023-02-13 19:59')
+        end
       end
     end
   end
@@ -31,35 +51,6 @@ RSpec.describe AuditsHelper, type: :helper do
 
     it 'formats date correctly' do
       expect(subject).to eq('2023-02-02 02:46')
-    end
-  end
-
-  describe '#verify_array' do
-    context 'when values is an array and attribute is "status_changed_at"' do
-      let(:attribute) { 'status_changed_at' }
-      let(:values) { ['2023-02-02TO2:46:30.352+03:00', '2023-02-13T19:59:17.202+03:00'] }
-
-      it 'returns formatted changes' do
-        expect(helper.verify_array(attribute, values)).to eq('2023-02-02 02:46 -> 2023-02-13 19:59')
-      end
-    end
-
-    context 'when values is an array and attribute is not "status_changed_at"' do
-      let(:attribute) { 'payment_status' }
-      let(:values) { %w[transferring confirming] }
-
-      it 'returns formatted changes' do
-        expect(helper.verify_array(attribute, values)).to eq('transferring -> confirming')
-      end
-    end
-
-    context 'when values is not an array' do
-      let(:attribute) { 'national_currency' }
-      let(:values) { 'RUB' }
-
-      it 'returns the original value' do
-        expect(helper.verify_array(attribute, values)).to eq('RUB')
-      end
     end
   end
 end
