2ed14958a4d5afe70db556112d3346dd0a1364a7
Spola1 <1111.22224@mail.ru>
2023-05-24 10:47:20 +0300

    update data migration

--- ppay_app/db/data/20230523094234_update_completed_payments.rb
+++ ppay_app/db/data/20230523094234_update_completed_payments.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UpdateCompletedPayments < ActiveRecord::Migration[7.0]
+  include Payments::Transactions::Base
+  
   def up
     Payment.where(payment_status: 'completed').each do |payment|
       calculate_national_currency_amount(payment)
@@ -14,8 +16,55 @@ class UpdateCompletedPayments < ActiveRecord::Migration[7.0]
 
   def calculate_national_currency_amount(payment)
     payment.transactions.each do |transaction|
-      transaction.national_currency_amount = transaction.amount
-      transaction.save!
+      if transaction.main? && payment.type == 'Deposit'
+        transaction.national_currency_amount = payment.national_currency_amount * main_transaction_percent / 100
+        transaction.save!
+      end
+      
+      if transaction.processer_commission? && payment.type == 'Deposit'
+        transaction.national_currency_amount = payment.national_currency_amount * processer_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.working_group_commission? && payment.type == 'Deposit'
+        transaction.national_currency_amount = payment.national_currency_amount * working_group_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.agent_commission? && payment.type == 'Deposit'
+        transaction.national_currency_amount = payment.national_currency_amount * agent_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.ppay_commission? && payment.type == 'Deposit'
+        transaction.national_currency_amount = payment.national_currency_amount * ppay_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.main? && payment.type == 'Withdrawal'
+        transaction.national_currency_amount = payment.national_currency_amount
+        transaction.save!
+      end
+      
+      if transaction.processer_commission? && payment.type == 'Withdrawal'
+        transaction.national_currency_amount = payment.national_currency_amount * processer_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.working_group_commission? && payment.type == 'Withdrawal'
+        transaction.national_currency_amount = payment.national_currency_amount * working_group_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.agent_commission? && payment.type == 'Withdrawal'
+        transaction.national_currency_amount = payment.national_currency_amount * agent_commission / 100
+        transaction.save!
+      end
+      
+      if transaction.ppay_commission? && payment.type == 'Withdrawal'
+        transaction.national_currency_amount = payment.national_currency_amount * ppay_commission / 100
+        transaction.save!
+      end
     end
   end
 end
