ae2bbef5f0e213a13cda09b9c753771646f66e87
Evgeniy Stashkovyan <stashevgenij@yandex.ru>
2023-05-24 23:11:09 +0300

    fiat balance fixes

--- ppay_app/app/helpers/transactions_helper.rb
+++ ppay_app/app/helpers/transactions_helper.rb
@@ -10,4 +10,18 @@ module TransactionsHelper
   def transaction_icon(transaction, balance)
     transaction.from_balance == balance ? 'arrow-down' : 'arrow-up'
   end
+
+  def crypto_address_label
+    national_balance? ? t('activerecord.attributes.balance_request.card_number') : 
+                        t('activerecord.attributes.balance_request.crypto_address')
+  end
+
+  def crypto_address_hint
+    national_balance? ? 'Номер карты, на которую вам перевести деньги' : 
+                        'Кошелёк, на который вам перевести деньги'
+  end
+
+  def national_balance?
+    current_user.is_a?(Merchant) && current_user.balance.in_national_currency?
+  end
 end
