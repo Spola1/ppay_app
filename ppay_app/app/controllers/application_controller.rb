8fb2e4ae9d439a4d59133b267184b60a0a181eb0
Spola1 <1111.22224@mail.ru>
2023-05-31 14:15:43 +0300

    transferring logic from application controller to payments controller && adding default lang from currency

--- ppay_app/app/controllers/application_controller.rb
+++ ppay_app/app/controllers/application_controller.rb
@@ -4,24 +4,11 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include Pagy::Backend
 
-  before_action :set_locale
-
   MANAGEMENT_NAMESPACES = %w[admins supports].freeze
 
   helper_method :role_namespace, :management_namespace?
 
   private
-
-  def set_locale
-    if params[:locale]
-      I18n.locale = params[:locale]
-      session[:locale] = params[:locale]
-    else
-      I18n.locale = session[:locale] || I18n.default_locale
-    end
-  rescue I18n::InvalidLocale
-    I18n.locale = I18n.default_locale
-  end
 
   def role_namespace
     current_user.type.underscore.pluralize if current_user
--- ppay_app/app/controllers/payments_controller.rb
+++ ppay_app/app/controllers/payments_controller.rb
@@ -3,12 +3,40 @@
 class PaymentsController < ApplicationController
   before_action :find_payment
   before_action :authenticate_signature
+  before_action :set_locale
 
   def show
     @payment.show! if @payment.may_show?
   end
 
   private
+
+  def set_locale
+    if params[:locale]
+      I18n.locale = params[:locale]
+      session[:locale] = params[:locale]
+    else
+      params[:locale] = locale_from_currency
+      I18n.locale = params[:locale]
+    end
+  rescue I18n::InvalidLocale
+    I18n.locale = I18n.default_locale
+  end
+
+  def locale_from_currency
+    locales = {
+      'RUB' => :ru,
+      'UZS' => :uz,
+      'TJS' => :tg,
+      'IDR' => :id,
+      'KZT' => :kk,
+      'UAH' => :uk,
+      'TRY' => :tr,
+      'KGS' => :ky
+    }
+
+    locales[@payment.national_currency]
+  end
 
   def find_payment
     @payment = model_class.constantize.find_by!(uuid: params[:uuid]).decorate
