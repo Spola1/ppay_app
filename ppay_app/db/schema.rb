277bccefeda34da5d6ab5799acb7645a41fefdc5
mveynberg <mv@cj264.ru>
2023-05-31 14:07:10 +0500

    Add payment_link with qr code to advertisements; Pass them to the api external_processing deposit creation response

--- ppay_app/db/schema.rb
+++ ppay_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_072205) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_161436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_072205) do
     t.boolean "hidden", default: false
     t.integer "account_id"
     t.bigint "processer_id"
+    t.string "payment_link"
     t.index ["processer_id"], name: "index_advertisements_on_processer_id"
   end
 
