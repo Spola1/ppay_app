3a680efae61358ce2b9c233652f98326c21b0714
Spola1 <1111.22224@mail.ru>
2023-07-10 17:59:22 +0300

    rename migration

--- ppay_app/db/migrate/20230710085113_add_form_customization_to_payments.rb
+++ ppay_app/db/migrate/20230710085113_add_form_customization_to_payments.rb
@@ -1,4 +1,4 @@
-class AddFormCustomizationIdToPayments < ActiveRecord::Migration[7.0]
+class AddFormCustomizationToPayments < ActiveRecord::Migration[7.0]
   def change
     add_reference :payments, :form_customization, foreign_key: true
   end
