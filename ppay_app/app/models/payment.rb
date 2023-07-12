80034cc24d9e33c815f00081b1d62a19583f9256
Spola1 <1111.22224@mail.ru>
2023-07-12 19:29:09 +0300

    upd views && model

--- ppay_app/app/models/payment.rb
+++ ppay_app/app/models/payment.rb
@@ -85,7 +85,6 @@ class Payment < ApplicationRecord
   after_update_commit lambda {
     broadcast_replace_payment_to_client if payment_status_previously_changed? || arbitration_previously_changed?
     broadcast_replace_payment_to_processer
-    broadcast_replace_payment_to_ad
     broadcast_replace_payment_to_support
   }
 
@@ -175,15 +174,6 @@ class Payment < ApplicationRecord
       partial: 'processers/payments/show_turbo_frame',
       locals: { payment: decorate, signature: nil, role_namespace: 'processers', can_manage_payment?: true },
       target: "processers_payment_#{uuid}"
-    )
-  end
-
-  def broadcast_replace_payment_to_ad
-    broadcast_replace_later_to(
-      "advertisements_payment_#{uuid}",
-      partial: 'processers/advertisements/show_turbo_frame',
-      locals: { advertisement: advertisement, payment: decorate, signature: nil, role_namespace: 'processers', can_manage_payment?: true },
-      target: "advertisements_payment_#{uuid}"
     )
   end
 
