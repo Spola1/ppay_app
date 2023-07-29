eb93081664e0382dbac226095b52aefb99cb2daa
Evgeniy Stashkovyan <stashevgenij@yandex.ru>
2023-07-29 17:37:07 +0300

    added destroy from not found payments

--- ppay_app/app/controllers/staff/management/not_found_payments_controller.rb
+++ ppay_app/app/controllers/staff/management/not_found_payments_controller.rb
@@ -3,7 +3,7 @@
 module Staff
   module Management
     class NotFoundPaymentsController < ApplicationController
-      before_action :find_not_found_payment, only: %i[show]
+      before_action :find_not_found_payment, only: %i[show destroy]
 
       def index
         @pagy, @not_found_payments = pagy(NotFoundPayment.order(created_at: :desc))
@@ -12,10 +12,18 @@ module Staff
 
       def show; end
 
+      def destroy
+        if @not_found_payment.destroy
+          redirect_to not_found_payments_path, notice: 'Платеж успешно удален'
+        else
+          redirect_to not_found_payments_path, alert: 'Ошибка удаления платежа'
+        end
+      end
+
       private
 
       def find_not_found_payment
-        @not_found_payment = NotFoundPayment.find(params[:id])
+        @not_found_payment = NotFoundPayment.find(params[:id]).decorate
       end
     end
   end
