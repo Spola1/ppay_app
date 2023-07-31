04aad4924d8b6f7a028362f7f61111dd33d0eb15
alexhowar <alexhowar>
2023-07-31 20:18:01 +0300

    byn card mask

--- ppay_app/app/javascript/controllers/inputmask/card_number_controller.js
+++ ppay_app/app/javascript/controllers/inputmask/card_number_controller.js
@@ -5,6 +5,28 @@ export default class extends Controller {
   static targets = ["input"]
 
   connect() {
-    Inputmask({ regex: "[\\d\\w]{4}( [\\d\\w]{4})*" }).mask(this.inputTargets)
+    this.applyInputMask();
+    this.loadPaymentSystem();
+  }
+
+  applyInputMask() {
+    const paymentSystem = this.data.get("paymentSystem");
+    if (this.isERIP(paymentSystem)) {
+      Inputmask({ regex: "[\\d\\w\\/]*" }).mask(this.inputTargets);
+    } else {
+      Inputmask({ regex: "[\\d\\w]{4}( [\\d\\w]{4})*" }).mask(this.inputTargets);
+    }
+  }
+
+  isERIP(paymentSystem) {
+    return ["ЕРИП БНБ", "ЕРИП Альфа", "ЕРИП Белагро"].includes(paymentSystem);
+  }
+
+  loadPaymentSystem() {
+    const paymentSystemSelect = document.getElementById("advertisement_payment_system");
+    paymentSystemSelect.addEventListener("change", (event) => {
+      this.data.set("paymentSystem", event.target.value);
+      this.applyInputMask();
+    });
   }
 }
