081ecd74996376d6aeabb9e900061cc719446c27
mveynberg <mv@cj264.ru>
2023-08-11 20:21:33 +0500

    Processer summary

--- ppay_app/app/decorators/balance_decorator.rb
+++ ppay_app/app/decorators/balance_decorator.rb
@@ -6,4 +6,12 @@ class BalanceDecorator < ApplicationDecorator
   def amount_formatted
     formatted_amount(amount)
   end
+
+  def frozen_amount_formatted
+    formatted_amount(from_transactions.payment_transactions.frozen.sum(:amount))
+  end
+
+  def today_income_formatted
+    formatted_amount(to_transactions.today.payment_transactions.processer_commission.completed.sum(:amount))
+  end
 end
