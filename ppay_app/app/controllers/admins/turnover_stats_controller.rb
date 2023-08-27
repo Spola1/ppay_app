19509c71df6dedd9dcd9a5f377d40544d5902ae6
Evgeniy Stashkovyan <stashevgenij@yandex.ru>
2023-08-27 18:55:12 +0300

    xlsx turnover fixes

--- ppay_app/app/controllers/admins/turnover_stats_controller.rb
+++ ppay_app/app/controllers/admins/turnover_stats_controller.rb
@@ -7,10 +7,11 @@ module Admins
     end
 
     def all_stats
+      @filtering_params = filtering_params || default_filtering_params
       respond_to do |format|
         format.xlsx do
           payments = Payment.all
-          render xlsx: 'all_stats', locals: { filtering_params: }, filename:
+          render xlsx: 'all_stats', locals: { filtering_params: @filtering_params }, filename:
         end
       end
     end
@@ -19,8 +20,8 @@ module Admins
 
     def filename
       "#{request.domain}_all_stats_" \
-      "#{filtering_params[:created_from].to_time&.strftime('%y%m%d')}-" \
-      "#{(filtering_params[:created_to].to_time || Time.zone.now).strftime('%y%m%d')}"
+      "#{@filtering_params[:created_from].to_time&.strftime('%y%m%d')}-" \
+      "#{(@filtering_params[:created_to].to_time || Time.zone.now).strftime('%y%m%d')}"
     end
 
     def set_all_merchants
@@ -32,6 +33,10 @@ module Admins
 
     def filtering_params
       params[:stats_filters]&.slice(:created_from, :created_to)
+    end
+
+    def default_filtering_params
+      { created_from: Time.zone.now.beginning_of_day, created_to: Time.zone.now.end_of_day }
     end
   end
 end
