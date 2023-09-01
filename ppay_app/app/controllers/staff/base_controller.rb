4a33385e2a6a6a56944f58e30cc9d745d29d2d1f
alexhowar <alexhowar>
2023-09-01 14:34:53 +0300

    comment headers && env

--- ppay_app/app/controllers/staff/base_controller.rb
+++ ppay_app/app/controllers/staff/base_controller.rb
@@ -13,11 +13,11 @@ module Staff
         cookie: request.cookies.to_h.to_s,
         url: request.original_url,
         method: request.method,
-        headers: request.headers.to_h.to_s,
+        #headers: request.headers.to_h.to_s,
         query_parameters: request.query_parameters.to_json,
         request_parameters: request.request_parameters.to_json,
         session: request.session.to_json,
-        env: request.env.to_s,
+        #env: request.env.to_s,
         ssl: request.ssl?
       )
     end
