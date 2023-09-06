03c81984f55f9a50d3e8ec66b240d3fe1e2215bb
Evgeniy Stashkovyan <stashevgenij@yandex.ru>
2023-09-06 11:18:01 +0300

    added initial_amount to api/v1/payments/show

--- ppay_app/app/serializers/api/v1/payments/show/base_serializer.rb
+++ ppay_app/app/serializers/api/v1/payments/show/base_serializer.rb
@@ -12,7 +12,7 @@ module Api
           attribute :uuid
           attribute :external_order_id, if: proc { |record| record.external_order_id.present? }
           attribute :cancellation_reason, if: proc { |record| record.cancellation_reason.present? }
-          attributes :created_at, :national_currency, :national_currency_amount,
+          attributes :created_at, :national_currency, :national_currency_amount, :initial_amount,
                      :cryptocurrency, :payment_system, :payment_status, :cryptocurrency_commission_amount,
                      :national_currency_commission_amount
         end
