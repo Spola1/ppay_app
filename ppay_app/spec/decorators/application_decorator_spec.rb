a88bdaddc34b646954d9157d22825da166dcfb9e
mveynberg <mv@cj264.ru>
2023-09-12 23:17:19 +0500

    Fix specs

--- ppay_app/spec/decorators/application_decorator_spec.rb
+++ ppay_app/spec/decorators/application_decorator_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationDecorator do
-  let(:payment) { create :payment, created_at: 'Fri, 17 Feb 2023 22:53:22.595096000 MSK +03:00' }
+  let(:exchange_portal) { create :exchange_portal, created_at: 'Fri, 17 Feb 2023 22:53:22.595096000 MSK +03:00' }
 
   describe '#formatted_created_at' do
     it 'Should return the correct created_at format' do
-      expect(payment.decorate.formatted_created_at).to eq('2023-02-17 22:53')
+      expect(exchange_portal.decorate.formatted_created_at).to eq('2023-02-17 22:53')
     end
   end
 end
