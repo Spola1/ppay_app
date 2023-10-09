0479fad0c471066288dda3f912be882db23b4599
mveynberg <mv@cj264.ru>
2023-10-09 17:34:56 +0500

    can_edit_summ null by default

--- ppay_app/db/migrate/20231009171337_add_can_edit_sum_to_operator.rb
+++ ppay_app/db/migrate/20231009171337_add_can_edit_sum_to_operator.rb
@@ -1,5 +1,5 @@
 class AddCanEditSumToOperator < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :can_edit_summ, :boolean, default: true
+    add_column :users, :can_edit_summ, :boolean
   end
 end
--- ppay_app/db/schema.rb
+++ ppay_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_062256) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_171337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -536,11 +536,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_062256) do
     t.integer "long_freeze_days"
     t.decimal "long_freeze_percentage", precision: 5, scale: 2
     t.integer "balance_freeze_type", default: 0
-    t.boolean "can_edit_summ", default: true
     t.string "otp_secret"
     t.integer "consumed_timestep"
     t.boolean "otp_required_for_login"
     t.boolean "otp_payment_confirm"
+    t.boolean "can_edit_summ"
     t.index ["agent_id"], name: "index_users_on_agent_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
