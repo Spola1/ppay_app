65228b5aa2de2e375e794364466c37d237b28940
mveynberg <mv@cj264.ru>
2023-10-27 15:15:45 +0500

    Bybit::UsertokenJob in_progress_lock

--- ppay_app/app/sidekiq/bybit/usertoken_job.rb
+++ ppay_app/app/sidekiq/bybit/usertoken_job.rb
@@ -6,21 +6,36 @@ module Bybit
     sidekiq_options queue: 'low', tags: ['bybit_usertoken']
 
     def perform
-      return if usertoken_valid?
+      return if usertoken_live?
       return unless ENV.fetch('BYBIT_EMAIL', nil)
 
-      bybit_portal.settings['usertoken'] = bybit_usertoken
-      bybit_portal.save if /\A[[:graph:]]+\Z/ =~ usertoken
+      in_progress_lock do
+        bybit_portal.settings['usertoken'] = bybit_usertoken
+        bybit_portal.save if /\A[[:graph:]]+\Z/ =~ usertoken
+      end
     end
 
     def bybit_portal = @bybit_portal ||= ExchangePortal.find_by_name('Bybit P2P')
 
     def usertoken = bybit_portal.settings['usertoken']
+    def usertoken_timestamp = bybit_portal.settings['usertoken']
 
     def otc = OtcOnline.new(usertoken)
 
-    def usertoken_valid? = otc.items({ pay_type: '75' }).present?
+    def usertoken_live? = otc.items({ pay_type: '75' }).present?
 
     def bybit_usertoken = `cd tmp; ../bin/bybit_usertoken`.strip
+
+    def in_progress_lock
+      bybit_portal.with_lock do
+        return if bybit_portal.in_progress
+
+        bybit_portal.update(in_progress: true)
+      end
+
+      yield
+    ensure
+      bybit_portal.update(in_progress: false)
+    end
   end
 end
