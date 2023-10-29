75f88283b9963fa33698470aff53a3f847ac8f6d
alexhowar <alexhowar>
2023-10-29 18:08:51 +0300

    refactoring

--- ppay_app/app/helpers/incoming_requests_helper.rb
+++ ppay_app/app/helpers/incoming_requests_helper.rb
@@ -4,4 +4,12 @@ module IncomingRequestsHelper
   def incoming_request_filters_params(key)
     params[:incoming_request_filters][key] if params[:incoming_request_filters]
   end
+
+  def incoming_request_filters_partial(user)
+    if user.processer?
+      'shared/staff/incoming_requests/filters'
+    else
+      'shared/staff/management/incoming_requests/filters'
+    end
+  end
 end
