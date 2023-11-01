304d2bedd2beb3b90cbbc32be757ccc57f6b28a3
Oleg <obp2000@mail.ru>
2023-11-01 19:46:26 +0300

    small fixes rev.2

--- ppay_app/spec/models/advertisement_spec.rb
+++ ppay_app/spec/models/advertisement_spec.rb
@@ -186,15 +186,6 @@ RSpec.describe Advertisement, type: :model do
       end
     end
 
-    # def advertisement_with_payments(payments_count: 3)
-    #   FactoryBot.create(:advertisement, status: false,
-    #                                     block_reason: :exceed_daily_usdt_card_limit,
-    #                                     card_number: '45678') do |advertisement|
-    #     FactoryBot.create(:processer, daily_usdt_card_limit: 300, advertisements: [advertisement])
-    #     FactoryBot.create_list(:payment, payments_count, payment_status: 'completed', advertisement:)
-    #   end
-    # end
-
     def processer_with_payments(payments_count: 3)
       FactoryBot.create(:processer, daily_usdt_card_limit: 100) do |processer|
         FactoryBot.create(:advertisement, status: false,
