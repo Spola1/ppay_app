2d1407fa08816cc4dcc92e15b114956f431b32ac
Oleg <obp2000@mail.ru>
2023-11-01 19:08:37 +0300

    for_enable_status

--- ppay_app/app/models/concerns/advertisement_scopes.rb
+++ ppay_app/app/models/concerns/advertisement_scopes.rb
@@ -93,12 +93,6 @@ module AdvertisementScopes
                       "THEN 1 ELSE 0 END) < #{limit}"))
     }
 
-    scope :with_limit_test, lambda {
-      joins(:processer).left_joins(:payments).merge(Payment.in_one_day.reorder(''))
-                       .select('advertisements.*').group('advertisements.id, users.id')
-                       .having('SUM(payments.cryptocurrency_amount) < users.daily_usdt_card_limit')
-    }
-
     scope :for_enable_status, lambda {
       where(status: false)
         .where(block_reason: :exceed_daily_usdt_card_limit)
