d1870564ca0e2b6b2a7806c326dc4bdb85cb3b8b
mveynberg <mv@cj264.ru>
2023-11-07 22:13:36 +0500

    Fix migration

--- ppay_app/db/migrate/20231101134013_add_incoming_requests_to_advertisement.rb
+++ ppay_app/db/migrate/20231101134013_add_incoming_requests_to_advertisement.rb
@@ -1,5 +1,7 @@
 class AddIncomingRequestsToAdvertisement < ActiveRecord::Migration[7.0]
-  change_table :advertisements do |t|
-    t.references :incoming_request, foreign_key: true
+  def change
+    change_table :advertisements do |t|
+      t.references :incoming_request, foreign_key: true
+    end
   end
 end
