321c9bf8541ccc323e9314c070cbf2f52d448d65
alexhowar <alexhowar>
2023-11-08 17:31:28 +0300

    10.seconds -> 605 seconds

--- ppay_app/app/decorators/telegram_connection_decorator.rb
+++ ppay_app/app/decorators/telegram_connection_decorator.rb
@@ -4,7 +4,7 @@ class TelegramConnectionDecorator < ApplicationDecorator
   delegate_all
 
   def status_with_color
-    if object.status.present? && object.status == 'success' && object.updated_at > 11.seconds.ago
+    if object.status.present? && object.status == 'success' && object.updated_at > 605.seconds.ago
       connected_status
     else
       not_connected_status
