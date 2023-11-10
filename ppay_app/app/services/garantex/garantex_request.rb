ca1e5bae258376a50f772a60c890fd5e074e58b6
Oleg <obp2000@mail.ru>
2023-11-10 09:45:38 +0300

    small fix

--- ppay_app/app/services/garantex/garantex_request.rb
+++ ppay_app/app/services/garantex/garantex_request.rb
@@ -20,27 +20,5 @@ module Garantex
       res = build_conn(token).get(url, form_data_hash)
       res.body
     end
-
-    # def self.send_get1(token, link, form_data_hash)
-    #   puts "send_get: #{link} #{form_data_hash}"
-
-    #   host = 'garantex.org'
-    #   custom_headers = { 'Authorization' => "Bearer #{token}" }
-    #   uri = URI.parse("https://#{host}/api/v2/#{link}")
-
-    #   response = Net::HTTP.start uri.hostname, uri.port, use_ssl: true,
-    #                                                      open_timeout: 60, read_timeout: 60 do |http|
-    #     request = Net::HTTP::Get.new uri.request_uri
-    #     custom_headers.each { |key, value| request[key] = value }
-    #     request.set_form_data(form_data_hash)
-    #     http.request request
-    #   end
-    #   case response
-    #   when Net::HTTPSuccess, Net::HTTPRedirection
-    #     JSON.parse(response.body)
-    #   else
-    #     response.value
-    #   end
-    # end
   end
 end
