9869d5600657a71955571cdfb6895ff6d12ddb82
alexeihowar <alexeihowar50@gmail.com>
2023-11-13 13:45:26 +0300

    tg restart

--- ppay_app/app/controllers/admins/telegram_applications_controller.rb
+++ ppay_app/app/controllers/admins/telegram_applications_controller.rb
@@ -3,7 +3,7 @@
 module Admins
   class TelegramApplicationsController < ApplicationController
     before_action :set_processers, only: %i[new create edit]
-    before_action :find_telegram_application, only: %i[edit show destroy update]
+    before_action :find_telegram_application, only: %i[edit show destroy update restart]
 
     def show; end
 
@@ -49,6 +49,14 @@ module Admins
       end
     end
 
+    def restart
+      @telegram_application.update(code: nil)
+
+      send_data_to_microservice(@telegram_application)
+
+      redirect_to edit_telegram_application_path(@telegram_application), notice: 'Приложение перезапущено. Доджитесь нового кода и сохраните его в соответствующее поле.'
+    end
+
     private
 
     def send_data_to_microservice(telegram_application)
@@ -66,6 +74,7 @@ module Admins
 
       send_request_to_microservice(data_to_send, 'create_telegram_application') if action_name == 'create'
       send_request_to_microservice(data_to_send, 'update_telegram_application') if action_name == 'update'
+      send_request_to_microservice(data_to_send, 'restart_telegram_application') if action_name == 'restart'
     end
 
     def send_request_to_microservice(data_to_send, endpoint)
