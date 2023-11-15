a4e844536c57ea575c5cea13f001f1a204d39ff1
mveynberg <mv@cj264.ru>
2023-11-15 13:10:19 +0500

    Clear huge app data from request dump to DB

--- ppay_app/app/controllers/payments_controller.rb
+++ ppay_app/app/controllers/payments_controller.rb
@@ -21,11 +21,13 @@ class PaymentsController < ApplicationController
       cookie: request.cookies.to_h.to_s,
       url: request.original_url,
       method: request.method,
-      headers: request.headers.to_h.to_s,
+      headers: request.headers.to_h
+        &.filter { !(/^(rack\.|action_dispatch\.|action_controller\.|puma\.|warden)/ =~ _1) }.to_s,
       query_parameters: request.query_parameters.to_json,
       request_parameters: request.request_parameters.to_json,
       session: request.session.to_json,
-      env: request.env.to_s,
+      env: request.env
+        &.filter { !(/^(rack\.|action_dispatch\.|action_controller\.|puma\.|warden)/ =~ _1) }.to_s,
       ssl: request.ssl?
     )
   end
