2f508dfc437ae2fbb43232aeab1b88c1905e4d08
mveynberg <mv@cj264.ru>
2023-12-01 21:14:13 +0500

    Fix values any

--- ppay_app/app/controllers/staff/management/advertisements_controller.rb
+++ ppay_app/app/controllers/staff/management/advertisements_controller.rb
@@ -6,7 +6,7 @@ module Staff
       before_action :find_advertisement, only: %i[show edit update destroy]
 
       def index
-        @advertisements = if filtering_params&.slice(:period, :created_from, :created_to)&.values&.any?(:present?)
+        @advertisements = if filtering_params&.slice(:period, :created_from, :created_to)&.values&.any?(&:present?)
                             Advertisement.time_filters(filtering_params)
                           else
                             Advertisement.filter_by(filtering_params)
